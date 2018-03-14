71ef32fb8be6c85ebed28741abad0a6278d21f23
Vincent Primault <v.primault@ucl.ac.uk>
2018-03-14 14:30:17 +0000

    Update license header on every file (Apache license)

--- node/ucl/pdd/chrome/app/background.js
+++ node/ucl/pdd/chrome/app/background.js
@@ -1,20 +1,17 @@
-//@flow
 /*
- * Private Data Donor is a platform to collect search logs via crowd-sourcing.
- * Copyright (C) 2017-2018 Vincent Primault <v.primault@ucl.ac.uk>
+ * Copyright 2017-2018 UCL / Vincent Primault <v.primault@ucl.ac.uk>
  *
- * Private Data Donor is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Private Data Donor is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with Private Data Donor.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import Raven from 'raven-js';
@@ -23,8 +20,6 @@ import moment from 'moment';
 import { history, storage } from './browser';
 import xhr from './util/xhr';
 import { encryptCounters, generateKeyPair } from './crypto';
-
-import type { Client, SubmitSketchCommand } from './types';
 
 // Configure Sentry reporting. The environment variables are provided at build time.
 Raven.config(process.env.SENTRY_DSN, { environment: process.env.NODE_ENV }).install();
@@ -74,13 +69,13 @@ function ping(client) {
     });
 }
 
-function getOrRegisterClient(): Promise<Client> {
+function getOrRegisterClient() {
   return storage.getClient().then(client => {
     return client || registerClient();
   });
 }
 
-function registerClient(): Promise<Client> {
+function registerClient() {
   console.log('Registering client...');
   const keyPair = generateKeyPair();
   const attrs = {
@@ -102,7 +97,7 @@ function registerClient(): Promise<Client> {
     });
 }
 
-function submitSketch(client: Client, command: SubmitSketchCommand): Promise<void> {
+function submitSketch(client, command) {
   const startTime = moment(command.startTime).valueOf();
   const endTime = moment(command.startTime).valueOf();
   return history
--- node/ucl/pdd/chrome/app/browser/Storage.js
+++ node/ucl/pdd/chrome/app/browser/Storage.js
@@ -1,32 +1,27 @@
-// @flow
 /*
- * Private Data Donor is a platform to collect search logs via crowd-sourcing.
- * Copyright (C) 2017-2018 Vincent Primault <v.primault@ucl.ac.uk>
+ * Copyright 2017-2018 UCL / Vincent Primault <v.primault@ucl.ac.uk>
  *
- * Private Data Donor is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Private Data Donor is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with Private Data Donor.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
-import type { Client } from '../types';
 
 // Key under which the data is actually stored inside Chrome local storage.
 const CLIENT_KEY = 'client';
 
 // In-memory cache of the data.
 let loaded = false;
-let localClient: ?Client = null;
+let localClient = null;
 
-function reload(): Promise<void> {
+function reload() {
   if (loaded) {
     return Promise.resolve();
   }
@@ -46,7 +41,7 @@ function reload(): Promise<void> {
   });
 }
 
-function write(key: string, item: any): Promise<void> {
+function write(key, item) {
   return new Promise((resolve, reject) => {
     chrome.storage.local.set({ [key]: JSON.stringify(item) }, () => {
       if (chrome.runtime.lastError) {
@@ -58,11 +53,11 @@ function write(key: string, item: any): Promise<void> {
   });
 }
 
-function getClient(): Promise<?Client> {
+function getClient() {
   return reload().then(() => localClient);
 }
 
-function setClient(client: Client): Promise<void> {
+function setClient(client) {
   return write(CLIENT_KEY, client).then(() => {
     localClient = client;
   });
