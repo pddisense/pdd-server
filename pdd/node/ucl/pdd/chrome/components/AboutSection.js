9d2f1c7a3455a31d3d6bbf63cc279e9026565ed8
Vincent Primault <v.primault@ucl.ac.uk>
2018-08-06 16:46:51 +0100

    Simplifies the About tab (avoids repetitions)

--- pdd/node/ucl/pdd/chrome/components/AboutSection.js
+++ pdd/node/ucl/pdd/chrome/components/AboutSection.js
@@ -28,29 +28,8 @@ export default class AboutSection extends React.Component {
           Privacy-friendly Data Donor is a tool connecting volunteers with researchers.
           The purpose of this extension is to study the spread of infectious diseases like flu.
           No information you provide will ever be used for commercial purposes or published elsewhere.
-          If you would like to learn more about our project, please visit <a
-          target="_blank" href="https://ppd.cs.ucl.ac.uk/">our website</a>.
-        </p>
-
-        <p>
           Towards this purpose, we monitor search queries that you make on Google.
           We are only interested in specific queries related to the diseases we are interested in.
-          This data collection works in two different modes.
-        </p>
-
-        <p>
-          <b>Encrypted mode</b>.
-          We are currently testing a privacy-preserving tool to collect how many users search for
-          certain keywords on Google, but not who. This makes it impossible for us to see individual
-          data related to each user; We can only see summed counts from groups of users.
-        </p>
-
-        <p>
-          <b>Raw mode</b>.
-          PDD also allows to collect raw individual search queries. Seeing raw data will help us
-          understand how our system works in practice and improve its operation before it is released
-          to the wider public. Even in this mode, we do not record the exact queries or the time at
-          which they were made, but only the number of times some specific keywords were searched.
         </p>
 
         <p>
