cb30fb0fcac50369cf2dd8269479536f9123e360
Vincent Primault <v.primault@ucl.ac.uk>
2018-11-29 13:58:48 +0000

    Fix docker publish script

--- bin/travis-deploy.sh
+++ bin/travis-deploy.sh
@@ -19,4 +19,5 @@ set -e
 
 # $DOCKER_USERNAME and $DOCKER_PASSWORD are provided by Travis CI.
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-sbt docker:publish
+sbt "project server" docker:publish
+sbt "project dashboard" docker:publish
